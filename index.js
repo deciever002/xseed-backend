05f4ad780a53c79a902546dd5306f65d4af47c9a
Manas Tiwari <manastiwari002@gmail.com>
2023-07-02 22:10:55 +0530

    modified origin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const passportLocalStrategy = require('./passportConfig')
 //initialized express app
 const app = express();
 app.use(cors({
-    origin: true,
+    origin: '*',
     credentials: true
 }))
 
